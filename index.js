54479159d5a22fb1d80130e28e97f7d04acecfd3
Harsh Tiwari <harshtiwari343@email.com>
2026-02-25 15:41:34 +0530

    Added mongodb CRUD APIs

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 let express = require("express");
-const app = express();
+const app = express(); // create express app
 const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Adds headers: Access-Control-Allow-Origin: *
 app.use(cors());
+app.use(bodyParser.json());
 
+// Mongodb connection
+mongoose.connect('mongodb://127.0.0.1:27017/feb26')
+    .then(() => console.log('Connected to Mongodb!'));
+
+// define schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String
+})
+
+// create model 
+const User = mongoose.model('User', userSchema);
+
+// default Routes
 app.get('/', (req, res) => {
     const users = [
         {
@@ -79,4 +96,67 @@ app.get('/', (req, res) => {
     res.json(users);
 })
 
-app.listen(3000);
+// READ: Get all users
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+// CREATE: Add new user
+app.post('/users', async (req, res) => {
+    try {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email
+        })
+        const savedUser = await user.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+})
+
+// READ: Get user by ID
+app.get('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return res.status(400).json({ message: 'User not found' });
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+// READ: Update user by ID
+app.put('/users/:id', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            { name: req.body.name, email: req.body.email },
+            { new: true } // return new user
+        )
+        if (!updatedUser) return res.status(404).json({ message: 'User not found' });
+        res.json(updatedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+})
+
+// DELETE: Delete user by ID
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) return res.status(404).json({ message: 'User not found' });
+        res.json({ message: 'user deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+})
+
+app.listen(3000, () => {
+    console.log(`Server started on port 3000`);
+});
